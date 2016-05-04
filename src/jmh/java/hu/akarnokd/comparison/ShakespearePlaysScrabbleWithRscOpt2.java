810b815d2882faf7a89d09ae6cd11ca3e51dc159
akarnokd <akarnokd@gmail.com>
2016-05-04 09:43:00 +0200

    Scrabble experiments

--- src/jmh/java/hu/akarnokd/comparison/ShakespearePlaysScrabbleWithRscOpt2.java
+++ src/jmh/java/hu/akarnokd/comparison/ShakespearePlaysScrabbleWithRscOpt2.java
@@ -18,22 +18,13 @@
 
 package hu.akarnokd.comparison;
 
-import java.util.ArrayList;
-import java.util.Comparator;
-import java.util.HashMap;
-import java.util.List;
+import java.util.*;
 import java.util.Map.Entry;
-import java.util.TreeMap;
 import java.util.concurrent.TimeUnit;
 import java.util.function.Function;
+import java.util.stream.Collectors;
 
-import org.openjdk.jmh.annotations.Benchmark;
-import org.openjdk.jmh.annotations.BenchmarkMode;
-import org.openjdk.jmh.annotations.Fork;
-import org.openjdk.jmh.annotations.Measurement;
-import org.openjdk.jmh.annotations.Mode;
-import org.openjdk.jmh.annotations.OutputTimeUnit;
-import org.openjdk.jmh.annotations.Warmup;
+import org.openjdk.jmh.annotations.*;
 
 import rsc.publisher.Px;
 
@@ -42,7 +33,7 @@ import rsc.publisher.Px;
  *
  * @author José
  */
-public class ShakespearePlaysScrabbleWithRscOpt extends ShakespearePlaysScrabble {
+public class ShakespearePlaysScrabbleWithRscOpt2 extends ShakespearePlaysScrabble {
 
 	
     static class LongWrapper {
@@ -117,11 +108,11 @@ public class ShakespearePlaysScrabbleWithRscOpt extends ShakespearePlaysScrabble
     	Function<Integer, Integer> scoreOfALetter = letter -> letterScores[letter - 'a'];
             
         // score of the same letters in a word
-        Function<Entry<Integer, LongWrapper>, Integer> letterScore =
+        Function<Entry<Integer, Long>, Integer> letterScore =
         		entry -> 
     					letterScores[entry.getKey() - 'a']*
     					Integer.min(
-    	                        (int)entry.getValue().get(), 
+    	                        entry.getValue().intValue(), 
     	                        (int)scrabbleAvailableLetters[entry.getKey() - 'a']
     	                    )
         	        ;
@@ -131,28 +122,21 @@ public class ShakespearePlaysScrabbleWithRscOpt extends ShakespearePlaysScrabble
         		string -> chars(string);
                     
         // Histogram of the letters in a given word
-        Function<String, Px<HashMap<Integer, LongWrapper>>> histoOfLetters =
+        Function<String, Px<Map<Integer, Long>>> histoOfLetters =
         		word -> toIntegerPx.apply(word)
-        					.collect(
-    							() -> new HashMap<Integer, LongWrapper>(), 
-    							(HashMap<Integer, LongWrapper> map, Integer value) -> 
-    								{ 
-    									LongWrapper newValue = map.get(value) ;
-    									if (newValue == null) {
-    										newValue = new LongWrapper();
-    										map.put(value, newValue);
-    									}
-    									newValue.incAndSet();
-    								}
-    								
-        					) ;
+        		.collect(
+                        Collectors.groupingBy(
+                            Function.identity(), 
+                            Collectors.counting()
+                        )
+                    );
                 
         // number of blanks for a given letter
-        Function<Entry<Integer, LongWrapper>, Long> blank =
+        Function<Entry<Integer, Long>, Long> blank =
         		entry ->
 	        			Long.max(
 	        				0L, 
-	        				entry.getValue().get() - 
+	        				entry.getValue() - 
 	        				scrabbleAvailableLetters[entry.getKey() - 'a']
 	        			)
         			;
@@ -219,18 +203,13 @@ public class ShakespearePlaysScrabbleWithRscOpt extends ShakespearePlaysScrabble
         Function<Function<String, Px<Integer>>, Px<TreeMap<Integer, List<String>>>> buildHistoOnScore =
         		score -> Px.fromIterable(shakespeareWords)
         						.filter(scrabbleWords::contains)
-        						.filter(word -> checkBlanks.apply(word).blockingFirst())
+        						.filter(word -> checkBlanks.apply(word).peekLast())
         						.collect(
-        							() -> new TreeMap<Integer, List<String>>(Comparator.reverseOrder()), 
-        							(TreeMap<Integer, List<String>> map, String word) -> {
-        								Integer key = score.apply(word).blockingFirst() ;
-        								List<String> list = map.get(key) ;
-        								if (list == null) {
-        									list = new ArrayList<String>() ;
-        									map.put(key, list) ;
-        								}
-        								list.add(word) ;
-        							}
+                                        Collectors.groupingBy(
+                                           word -> score.apply(word).peekLast(), 
+                                           () -> new TreeMap<Integer, List<String>>(Comparator.reverseOrder()), 
+                                           Collectors.toList()
+                                        )
         						) ;
                 
         // best key / value pairs
@@ -238,13 +217,8 @@ public class ShakespearePlaysScrabbleWithRscOpt extends ShakespearePlaysScrabble
         		buildHistoOnScore.apply(score3)
         			.flatMapIterable(map -> map.entrySet())
         			.take(3)
-        			.collect(
-        				() -> new ArrayList<Entry<Integer, List<String>>>(), 
-        				(list, entry) -> {
-        					list.add(entry) ;
-        				}
-        			)
-        			.blockingFirst() ;
+        			.collect(Collectors.toList()) 
+        			.peekLast() ;
         			
         
 //        System.out.println(finalList2);
@@ -253,7 +227,7 @@ public class ShakespearePlaysScrabbleWithRscOpt extends ShakespearePlaysScrabble
     }
     
     public static void main(String[] args) throws Exception {
-        ShakespearePlaysScrabbleWithRscOpt s = new ShakespearePlaysScrabbleWithRscOpt();
+        ShakespearePlaysScrabbleWithRscOpt2 s = new ShakespearePlaysScrabbleWithRscOpt2();
         s.init();
         System.out.println(s.measureThroughput());
     }
