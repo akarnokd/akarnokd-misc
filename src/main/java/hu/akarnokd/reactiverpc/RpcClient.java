f3e38db68ec770f530f6333cf72a8d2f67d0fed3
akarnokd <akarnokd@gmail.com>
2016-05-23 16:53:19 +0200

    Cleint/Server class some methods

--- src/main/java/hu/akarnokd/reactiverpc/RpcClient.java
+++ src/main/java/hu/akarnokd/reactiverpc/RpcClient.java
@@ -2,29 +2,32 @@ package hu.akarnokd.reactiverpc;
 
 import java.net.InetAddress;
 import java.util.Objects;
+import java.util.function.Consumer;
 
-public class RpcClient<T extends AutoCloseable> {
+import hu.akarnokd.rxjava2.disposables.Disposable;
+
+public final class RpcClient<T> {
 
     private RpcClient(Class<T> remoteAPI, Object localAPI) {
     }
     
-    public static RpcClient<AutoCloseable> createLocal(Object localAPI) {
+    public static RpcClient<Void> createLocal(Object localAPI) {
         Objects.requireNonNull(localAPI, "localAPI");
         return new RpcClient<>(null, localAPI);
     }
     
-    public static <T extends AutoCloseable> RpcClient<T> createRemote(Class<T> remoteAPI) {
+    public static <T> RpcClient<T> createRemote(Class<T> remoteAPI) {
         Objects.requireNonNull(remoteAPI, "remoteAPI");
         return new RpcClient<>(remoteAPI, null);
     }
     
-    public static <T extends AutoCloseable> RpcClient<T> createBidirectional(Class<T> remoteAPI, Object localAPI) {
+    public static <T> RpcClient<T> createBidirectional(Class<T> remoteAPI, Object localAPI) {
         Objects.requireNonNull(remoteAPI, "remoteAPI");
         Objects.requireNonNull(localAPI, "localAPI");
         return new RpcClient<>(remoteAPI, localAPI);
     }
     
-    public T connect(InetAddress endpoint, int port) {
+    public T connect(InetAddress endpoint, int port, Consumer<Disposable> close) {
         return null;
     }
 }
