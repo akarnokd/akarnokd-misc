4ef8d72dea510fc29aa45813464daadf5f0035e0
akarnokd <akarnokd@gmail.com>
2016-10-04 15:36:13 +0200

    testing an odd call

---
+++ src/main/java/hu/akarnokd/rxjava2/OddCall.java
@@ -0,0 +1,50 @@
+package hu.akarnokd.rxjava2;
+
+import io.reactivex.*;
+import io.reactivex.disposables.Disposable;
+import io.reactivex.functions.*;
+import io.reactivex.subjects.PublishSubject;
+
+public class OddCall {
+
+    public static void main(String[] args) {
+        PublishSubject<String> trigger = PublishSubject.create();
+        
+        Observable<String> obs1 = trigger.filter(new Predicate<String>(){
+            @Override
+            public boolean test(String s) throws Exception{
+                return s != null;
+            }
+        });
+
+        Observable<String> obs2 = Observable.create(new ObservableOnSubscribe<String>(){
+            @Override
+            public void subscribe(final ObservableEmitter<String> e) throws Exception {
+                //When/is this ever disposed? 
+                obs1.subscribe(new Consumer<String>(){
+                    @Override
+                    public void accept(String s){
+                        System.out.println(s);
+                        //Never called after obs1 is disposed.
+                        e.onNext(s);
+                    }
+                });
+            }
+        });
+
+        //A
+        Disposable disposable = obs2.subscribe(new Consumer<String>(){
+            @Override
+            public void accept(String s){
+                System.out.println(s);
+            }
+        });
+
+
+        trigger.onNext("Holahoop"); //Above "A" is called.
+
+        disposable.dispose();
+
+        trigger.onNext("Hakuna Matata"); //Nothing happens. 
+    }
+}
