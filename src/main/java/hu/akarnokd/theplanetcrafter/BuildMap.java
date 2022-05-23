5dd35eb73cb3b77c357d97b647d5bfaa11adbf71
akarnokd <akarnokd@gmail.com>
2022-05-23 16:01:25 +0200

    Use smaller tiles

--- src/main/java/hu/akarnokd/theplanetcrafter/BuildMap.java
+++ src/main/java/hu/akarnokd/theplanetcrafter/BuildMap.java
@@ -1,8 +1,11 @@
 package hu.akarnokd.theplanetcrafter;
 
-import java.awt.*;
+import java.awt.Graphics2D;
+import java.awt.RenderingHints;
 import java.awt.image.BufferedImage;
 import java.io.File;
+import java.util.HashMap;
+import java.util.Map;
 
 import javax.imageio.ImageIO;
 
@@ -12,22 +15,29 @@ public class BuildMap {
         buildMapOf("barren");
         buildMapOf("lush");
     }
+    
+    record Coords(int x, int z) { }
 
     static void buildMapOf(String mode) throws Exception {
 
-        int step = 200;
+        int step = 100;
         int maxZ = 3000;
         int minZ = -1400;
         int maxX = 2400;
         int minX = -1000;
+        int defaultY = 50;
 
         int nHorizontal = (maxZ - minZ) / step;
         int nVertical = (maxX - minX) / step;
 
-        int w = 80;
-        int h = 60;
+        int w = 40;
+        int h = 30;
+        Map<Coords, Integer> heightMap = new HashMap<>();
+        // customize which height to pick from the map photos
+        
+        // --------------------------------------------------
 
-        BufferedImage bimg = new BufferedImage(w * nHorizontal, h * nVertical, BufferedImage.TYPE_INT_ARGB);
+        BufferedImage bimg = new BufferedImage(w * nHorizontal, h * nVertical, BufferedImage.TYPE_INT_RGB);
         Graphics2D g2 = bimg.createGraphics();
         g2.setRenderingHint(RenderingHints.KEY_INTERPOLATION, RenderingHints.VALUE_INTERPOLATION_BILINEAR);
 
@@ -37,11 +47,15 @@ public class BuildMap {
             int outy = 0;
             for (int x = maxX; x >= minX; x -= step) {
 
-                //System.out.println(mode + " - " + z + " : " + x);
+                System.out.println(mode + " - " + z + " : " + x);
+                int y = heightMap.getOrDefault(new Coords(x, z), defaultY);
 
-                BufferedImage image = ImageIO.read(new File("c:\\temp\\" + mode + "\\map_" + z + "_" + x + ".png"));
+                BufferedImage image = ImageIO.read(new File("c:\\temp\\" + mode + "\\map_" + z + "_" + x + "_" + y + ".png"));
 
-                g2.drawImage(image, outx - w / 2, outy - h / 2, outx + w / 2, outy + h / 2, 0, 0, image.getWidth(), image.getHeight(), null);
+                int iw = image.getWidth();
+                int ih = image.getHeight();
+                
+                g2.drawImage(image, outx - w / 2, outy - h / 2, outx + w / 2, outy + h / 2, iw / 4, ih / 4, iw * 3 / 4, ih * 3 / 4, null);
 
                 outy += h;
             }
